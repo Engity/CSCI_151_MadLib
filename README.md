afa46f258e62390de0951b14a3c1a05345ba1369
Engity <ngtoan1107@gmail.com>
2021-10-06 20:24:43 -0700

    Initial Release

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# CSCI_151_Matlib
+# CSCI_151_MadLib
+Instruction
+The story is written in story.txt file
+The gaps are identified using a pair of <>
+Between this pair is what type of word the user should be replacing
+Example: <proper noun> went to the <place> to get some <thing>
+
+
+#About Mad libs
+A mad lib is a word game where you take a short story (usually only a paragraph long) and remove several words from it and replace them with alternative words with matching grammatical structure. The fun comes from the fact that the replacement words are provided by somebody who has not yet read the story, and so once the story is re-composed, it becomes silly and non-sensical.
+
+For example, if you have the sentence:
+    
+_Johnny went to the store to get some milk._
+
+You might remove the following three words: "Johnny", "store", "milk". _Johnny_ is a proper noun,  _store_ is a place, and _milk_ is a thing. The sentence with words removed becomes:
+
+__________ went to the _________ to get some ________.
+
+You would then ask your friend to provide alternatives for these words by only telling him or her the type of word you are looking for. You might say: "give me a proper noun" and he or she would give you the first name that comes to mind: "Sam". The you might say "give me a place" and he or she might say "The Grand Canyon". Finally you would ask "give me a thing" and he or she might say "lawnmower".
+
+You then re-compose the story by injecting the words provided by your friend. Our hypothetical mad lib becomes:
+
+_Sam went to the Grand Canyon to get a lawnmower._
+
+## Project Requirements
+1. Find a mad lib online, in a book, or write one yourself. You need to have at least _10_ replacement words.
+2. Reproduce the mad lib game with a program which plays the role of the story editor.
+   1. The program will ask a series of questions at the console and the user of the program will provide the answers to those questions. 
+   2. Finally the program will print out the full mad lib with the input words replaced into the story
+
+For example:
+   ```
+   Give me a proper noun: Sam
+   Give me a place: Grand Canyon
+   Give me a thing: Lawnmower
+   Sam went to the Grand Canyon to get a Lawnmower.
---
+++ mad_lib.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long ld;
+#define FOR(i,a,b)\
+	for (auto i = a; i != b; i++)
+#define fi first
+#define se second
+
+//Store all the words
+vector <string> words, user_words;
+//Storing where the replacing words are
+vector <ld> replacing_words;
+
+void inputingStory(){
+    ifstream inp("story.txt");
+    string word;
+
+    while (!inp.eof()){
+        string sentence;
+        
+        //Read the whole sentence and break them into words
+        getline(inp, sentence);
+        word = "";
+        //Check if reading a gap or not
+        bool readingGap = 0;
+        FOR(i,0,sentence.size()){
+            //Eliminating double space
+            if (sentence[i] == ' ' && (i != sentence.size() - 1) && sentence[i + 1] == ' '){
+                continue;
+            }
+
+            if (sentence[i] == '<'){
+                    replacing_words.push_back(words.size());
+                    readingGap = 1;
+            }
+
+            word += sentence[i];
+            if (sentence[i] == '>'){
+                    words.push_back(word);
+                    word = "";
+                    readingGap = 0;
+            }
+            //if space is detected, break the words
+            if (sentence[i] == ' ' && !readingGap){
+                words.push_back(word);
+                word = "";
+            }
+
+           
+        }
+        //Inputing the last word
+        if (word[0] == '<'){
+            replacing_words.push_back(words.size());  
+        }
+        words.push_back(word + "\n");     
+    }
+
+    inp.close();
+}
+//Removing the bracket in in the words we are supposed to replace
+string removeBracket(string x){
+    string res = "";
+    //Go through them one by one
+    /*FOR(i,0, x.size()){
+        if (x[i] == '>'){
+            return res;
+        }
+        if (x[i] == '<'){
+            continue;
+        }
+
+        res += x[i];;
+    }*/
+
+    res = x.substr(1, x.size() - 2);
+    return res;
+}
+//Inputing user's words
+void inputReplacingWords(){
+    FOR(i,0,replacing_words.size()){
+        string x = words[replacing_words[i]];
+        cout << "Give me a(n) " << removeBracket(x) << "\n";
+        cin >> x;
+        user_words.push_back(x);
+    }
+}
+
+//Replacing them
+void replaceWords(){
+    FOR(i,0,user_words.size()){
+        words[replacing_words[i]] = user_words[i];
+    }
+}
+
+int main(){
+    inputingStory();
+    inputReplacingWords();
+    replaceWords();
+
+    cout << "Here is the new story!!!\n";
+    FOR(i,0,words.size()){
+        cout << words[i] ;
+    }
+
+    system("pause");
+    return 0;
+}
